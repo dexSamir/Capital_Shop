0fe0dc524d9faa56f1acd0014e1527660963c955
dexSamir <samirah-bp215@code.edu.az>
2025-04-22 23:52:21 +0400

    create api side, build n-tier archtecture

---
+++ API/Capital.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Capital.Core.Entities.Base;
+
+public abstract class BaseEntity
+{
+    public Guid Id{ get; set; }
+    public DateTime CreatedTime { get; set; }
+    public DateTime? UpdatedTime { get; set; }
+    public bool isDeleted { get; set; }
+    public bool isUpdated { get; set; }
+}
+
---
+++ API/Capital.Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using Capital.Core.Entities.Base;
+
+namespace Capital.Core.Entities;
+
+public class Product : BaseEntity
+{
+	public decimal SellPrice { get; set; }
+	public decimal CostPrice { get; set; }
+	public int Quantity {get; set;}
+	public string Title { get; set; } = null!;
+	public string Description { get; set; } = null!;
+	public string CoverImage { get; set; } = null!;
+
+	//Category
+
+	//OtherImages
+
+	//Reviews
+
+
+
+}
+
