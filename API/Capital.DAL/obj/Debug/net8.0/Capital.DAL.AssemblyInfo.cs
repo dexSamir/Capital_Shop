4fde058493ee010f57482059e283382ed889aba2
dexSamir <samirah-bp215@code.edu.az>
2025-04-27 18:55:47 +0400

    done repository

--- API/Capital.DAL/obj/Debug/net8.0/Capital.DAL.AssemblyInfo.cs
+++ API/Capital.DAL/obj/Debug/net8.0/Capital.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capital.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721e8237965cff3f36193c7fa825b0fc4e3521c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d05dc50e641456fe574a96820288a6f76ece60f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capital.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capital.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
