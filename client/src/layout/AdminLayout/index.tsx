55dc4c530e00d89b4faa86464e9f9af622acc146
dexSamir <samirah-bp215@code.edu.az>
2026-03-22 17:03:54 +0400

    update admin panel ui

--- client/src/layout/AdminLayout/index.tsx
+++ client/src/layout/AdminLayout/index.tsx
@@ -1,18 +1,12 @@
 import { Outlet } from "react-router-dom"
-import Navbar from "../../components/Navbar"
-import Footer from "../../components/Footer"
-import Header from "../../components/Header"
 import "./AdminLayout.scss"
 
 function AdminLayout() {
   return (
     <div className="admin-layout">
-      <Navbar />
-      <Header />
       <main className="admin-layout__main">
         <Outlet />
       </main>
-      <Footer />
     </div>
   )
 }
